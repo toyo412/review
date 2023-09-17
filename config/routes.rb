47d22ee92ff3559842613b6a2e353d5398254261
accel <ttttytt412@gmail.com>
2023-09-17 12:04:43 +0900

    gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/", to:'staticpages#home'
+  get "/help", to:'staticpages#help'
+  get "/contact", to:'staticpages#contact'
 end
---
+++ test/controllers/staticpages_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class StaticpagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get staticpages_home_url
+    assert_response :success
+  end
+
+  test "should get help" do
+    get staticpages_help_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get staticpages_contact_url
+    assert_response :success
+  end
+end
